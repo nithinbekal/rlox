b47fb843cad0178ba20aa397baf4866638d66128
Nithin Bekal <nithinbekal@gmail.com>
2026-02-02 01:56:13 -0500

    Add Rlox.run method

--- lib/rlox.rb
+++ lib/rlox.rb
@@ -12,4 +12,16 @@ require_relative "rlox/interpreter"
 module Rlox
   class Error < StandardError; end
   class ParserError < Error; end
+
+  def self.run(source)
+    scanner = Rlox::Scanner.new(source)
+    tokens = scanner.scan_tokens
+    parser = Rlox::Parser.new(tokens)
+    expressions = parser.parse
+
+    return if expressions.nil?
+
+    interpreter = Rlox::Interpreter.new
+    interpreter.evaluate(expressions.first)
+  end
 end
