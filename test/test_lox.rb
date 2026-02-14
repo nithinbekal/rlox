05b5d48db3f688a9c7990519703ca89ebae498c9
Nithin Bekal <nithinbekal@gmail.com>
2026-02-14 00:56:37 -0500

    fix tests

--- test/test_lox.rb
+++ test/test_lox.rb
@@ -5,43 +5,53 @@ require "test_helper"
 module Rlox
   class TestLox < Minitest::Test
     def test_run_literal
-      assert_equal 1, Rlox.run("1")
+      assert_equal 1, Rlox.run("1;")
     end
 
     def test_arithmetic_expressions
-      assert_equal 3, Rlox.run("1 + 2")
-      assert_equal(-1, Rlox.run("1 - 2"))
-      assert_equal 2, Rlox.run("1 * 2")
-      assert_equal 0.5, Rlox.run("1 / 2")
+      assert_equal 3, Rlox.run("1 + 2;")
+      assert_equal(-1, Rlox.run("1 - 2;"))
+      assert_equal 2, Rlox.run("1 * 2;")
+      assert_equal 0.5, Rlox.run("1 / 2;")
     end
 
     def test_arithmetic_expressions_with_multiple_operations
-      assert_equal 7, Rlox.run("1 + 2 * 3")
-      assert_equal 5, Rlox.run("1 * 2 + 3")
-      assert_equal 11, Rlox.run("1 + 2 * 3 + 4")
-      assert_equal 14, Rlox.run("1 * 2 + 3 * 4")
+      assert_equal 7, Rlox.run("1 + 2 * 3;")
+      assert_equal 5, Rlox.run("1 * 2 + 3;")
+      assert_equal 11, Rlox.run("1 + 2 * 3 + 4;")
+      assert_equal 14, Rlox.run("1 * 2 + 3 * 4;")
     end
 
     def test_comparison_expressions
-      assert_equal true, Rlox.run("1 < 2")
-      assert_equal true, Rlox.run("1 <= 2")
-      assert_equal false, Rlox.run("1 > 2")
-      assert_equal false, Rlox.run("1 >= 2")
-      assert_equal true, Rlox.run("1 == 1")
-      assert_equal true, Rlox.run("1 != 2")
+      assert_equal true, Rlox.run("1 < 2;")
+      assert_equal true, Rlox.run("1 <= 2;")
+      assert_equal false, Rlox.run("1 > 2;")
+      assert_equal false, Rlox.run("1 >= 2;")
+      assert_equal true, Rlox.run("1 == 1;")
+      assert_equal true, Rlox.run("1 != 2;")
     end
 
     def test_grouped_expressions
-      assert_equal 9, Rlox.run("(1 + 2) * 3")
-      assert_equal 7, Rlox.run("1 + (2 * 3)")
-      assert_equal 24, Rlox.run("2 + (3 * 5) + 7")
+      assert_equal 9, Rlox.run("(1 + 2) * 3;")
+      assert_equal 7, Rlox.run("1 + (2 * 3);")
+      assert_equal 24, Rlox.run("2 + (3 * 5) + 7;")
     end
 
     def test_unary_expressions
-      assert_equal(-1, Rlox.run("-1"))
-      assert_equal true, Rlox.run("!false")
-      assert_equal false, Rlox.run("!true")
-      assert_equal true, Rlox.run("!nil")
+      assert_equal(-1, Rlox.run("-1;"))
+      assert_equal true, Rlox.run("!false;")
+      assert_equal false, Rlox.run("!true;")
+      assert_equal true, Rlox.run("!nil;")
+    end
+
+    def test_print_statement
+      output = capture_io { Rlox.run("print 42;") }
+      assert_equal "42\n", output[0]
+    end
+
+    def test_multiple_statements
+      output = capture_io { Rlox.run("print 1; print 2;") }
+      assert_equal "1\n2\n", output[0]
     end
   end
 end
